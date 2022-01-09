9a009fd9775883f01c8402cd92410198d500bc75
flashbtw <johannes-kurt.reiter@freenet.de>
2022-01-09 20:24:16 +0100

    working version

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function groessterGemeinsamerTeiler(teiler1,teiler2) {
       t1=t2;
       t2=rest;
     } while (rest>0);
-    document.getElementById("ggT").innerText = t1.toString();
     return t1;
 
 }
@@ -17,11 +16,50 @@ function kleinstesGemeinsamesVielfaches(number1,number2) {
     n2 = parseInt(number2);
     ggt = groessterGemeinsamerTeiler(n1, n2);
     kgV = (n1 * n2) / ggt;
-    document.getElementById("kgv").innerText = kgV.toString();
     return kgV;
 
 }
 
-function bruecheKuerzen {
+function bruchAddierenUndKuerzen(br1o, br1u, br2o, br2u) {
+
     
+        b1o = parseInt(br1o);
+        b1u = parseInt(br1u);
+        b2o = parseInt(br2o);
+        b2u = parseInt(br2u);
+    if (validationOfNumbers(b1o, b1u, b2o, b2u)) {
+        kgV_Nenner = kleinstesGemeinsamesVielfaches(b1u, b2u);
+        faktorBruch1 = kgV_Nenner / b1u;
+        faktorBruch2 = kgV_Nenner / b2u;
+    
+        b1o = b1o * faktorBruch1;
+        b1u = b1u * faktorBruch1;
+        b2o = b2o * faktorBruch2;
+        b2u = b2u * faktorBruch2;
+        bo = b1o + b2o;
+        bu = b1u
+        ggtZumKuerzen = groessterGemeinsamerTeiler(bo,bu);
+        bo = bo / ggtZumKuerzen;
+        bu = bu / ggtZumKuerzen;
+
+        document.getElementById("eo").innerHTML = bo;
+        document.getElementById("eu").innerHTML = bu;
+    } else {
+        document.getElementById("errors").innerText = "Zahl(en) nicht angegeben oder nicht zulässig"
+    }
+}
+
+function validationOfNumbers(n1, n2, n3, n4) {
+    let valid = true;
+
+    if (isNaN(n1)) {
+        valid = false;
+    } else if (isNaN(n2)) {
+        valid = false;
+    } else if (isNaN(n3)) {
+        valid = false;
+    } else if (isNaN(n4)) {
+        valid = false;
+    }
+    return valid;
 }
